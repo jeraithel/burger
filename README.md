e612c526b5b78cfd1fd635037170f725bd5f3b9d
Justin Raithel <jeraithel@yahoo.com>
2019-06-22 18:51:08 -0700

    working application

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# burger
+##Project Name: Burger Devourer
+#Description: 
+User can add burgers and devour them.    
+#Technology: 
+MySQL, Express, Node, Handlebars
+
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("double royale with cheese");
+
+INSERT INTO burgers (burger_name)
+VALUES ("cootie burger");
